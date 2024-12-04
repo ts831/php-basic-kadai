155f18004fbed31788636d6f479745a8ec619425
ts831 <ts81184@gmail.com>
2024-12-04 21:20:42 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+ <html lang="ja">
+ 
+ <head>
+     <meta charset="UTF-8">
+     <title>kadai_007</title>
+ </head>
+ 
+ <body>
+     <p>
+         <?php
+
+            $data['name']='onion';
+
+            $data['price']=200;
+
+            $data['weight']=160;
+
+            echo print_r($data);
+         ?>
+     </p>
+ </body>
+ 
+ </html>
